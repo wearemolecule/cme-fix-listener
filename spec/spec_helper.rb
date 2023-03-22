80fe0356667999560ab8303d198a9a928b057d7c
Steve Cooke <steve@molecule.io>
2023-03-22 16:33:39 -0400

    adding REDIS_HOST env var to github actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,9 @@ require_all "lib"
 Time.zone = "Central Time (US & Canada)"
 
 RSpec.configure do |config|
+  config.before(:all) do
+    Resque.redis = Redis.new(host: ENV["REDIS_HOST"], port: ENV["REDIS_PORT"])
+  end
   config.before(:each) do
     some_logger = double("some logger").as_null_object
     allow(Logging).to receive(:logger).and_return(some_logger)
